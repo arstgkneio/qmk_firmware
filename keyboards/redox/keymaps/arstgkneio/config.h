d770e81d18c257f836f2c43732027a33d287241e
arstgkneio <andreygorinov@gmail.com>
2024-10-24 06:29:50 -0400

    Add EE_HANDS definition to config.h for dynamic split keyboard handedness

--- keyboards/redox/keymaps/arstgkneio/config.h
+++ keyboards/redox/keymaps/arstgkneio/config.h
@@ -17,6 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/* Use I2C or Serial, not both */
+#define USE_SERIAL
+
+#define EE_HANDS
+
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
